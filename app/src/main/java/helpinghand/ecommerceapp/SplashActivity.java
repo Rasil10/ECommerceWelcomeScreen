a44af147ca923632d4b7bb943559e2f5c21c919e
Rasil10 <rasilr10@gmail.com>
2017-12-09 21:47:29 +0545

    Initial commit

---
+++ app/src/main/java/helpinghand/ecommerceapp/SplashActivity.java
@@ -0,0 +1,24 @@
+package helpinghand.ecommerceapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        overridePendingTransition( R.anim.slide_in_up, R.anim.slide_out_up );
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
